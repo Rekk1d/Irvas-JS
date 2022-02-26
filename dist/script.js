dd7f42e5920016c7685600f34ec0917d8e74c8af
Misha <xxxdaddy420@gmail.com>
2022-02-26 16:54:22 +0700

    fixed bug with with the opening modal

--- dist/script.js
+++ dist/script.js
@@ -18099,7 +18099,8 @@ var modals = function modals() {
     var trigger = document.querySelectorAll(triggerSelector),
         modal = document.querySelector(modalSelector),
         close = document.querySelector(closeSelector),
-        windows = document.querySelectorAll('[data-modal]');
+        windows = document.querySelectorAll('[data-modal]'),
+        scroll = calcScroll();
     trigger.forEach(function (item) {
       item.addEventListener('click', function (e) {
         if (e.target) {
@@ -18111,12 +18112,14 @@ var modals = function modals() {
         });
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = "".concat(scroll, "px");
       });
     });
 
     function closeModal() {
       modal.style.display = 'none';
       document.body.style.overflow = '';
+      document.body.style.marginRight = "0px";
       windows.forEach(function (item) {
         item.style.display = 'none';
       });
@@ -18138,6 +18141,18 @@ var modals = function modals() {
       document.querySelector(selector).style.display = 'block';
       document.body.style.overflow = 'hidden';
     }, time);
+  }
+
+  function calcScroll() {
+    var div = document.createElement('div');
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+    document.body.appendChild(div);
+    var scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+    return scrollWidth;
   }
 
   bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
